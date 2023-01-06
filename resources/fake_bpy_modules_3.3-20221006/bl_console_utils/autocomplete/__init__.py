49655f853f50e4c3d2c81dbfb5d4265e266c9c9d
TBA <84085705+TBAndrade@users.noreply.github.com>
2023-01-06 09:54:00 +0000

    Reorder files

--- resources/fake_bpy_modules_3.3-20221006/bl_console_utils/autocomplete/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-import typing
-from . import complete_import
-from . import complete_calltip
-from . import intellisense
-from . import complete_namespace
-
-GenericType = typing.TypeVar("GenericType")
--- resources/fake_bpy_modules_3.3-20221006/bl_keymap_utils/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-import typing
-from . import keymap_hierarchy
-from . import keymap_from_toolbar
-from . import io
-from . import platform_helpers
-from . import versioning
-
-GenericType = typing.TypeVar("GenericType")
