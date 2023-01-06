6823a4ba1769ec618e8d5622e2fdb8d880736dc0
TBA <84085705+TBAndrade@users.noreply.github.com>
2023-01-06 17:16:42 +0000

    New version to check on remote

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ bl_info = {
     "author" : "Tiago Andrade",
     "description" : "",
     "blender" : (3, 4, 0),
-    "version" : (1, 2, 0),
+    "version" : (1, 2, 1),
     "location" : "Topbar",
     "warning" : "",
     "category" : "Object"
