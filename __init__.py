d299af83abecd1ad0d7abcfa1995070b674bd6c9
TBA <84085705+TBAndrade@users.noreply.github.com>
2023-02-06 09:19:38 +0000

    Addon version update

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ bl_info = {
     "author" : "Tiago Andrade",
     "description" : "",
     "blender" : (3, 4, 1),
-    "version" : (1, 3, 3),
+    "version" : (1, 3, 4),
     "location" : "Topbar",
     "warning" : "",
     "category" : "Object"
