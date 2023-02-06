51353b1c052203a2e6e2adbc00e423d89f132f1f
TBA <84085705+TBAndrade@users.noreply.github.com>
2023-02-06 11:01:35 +0000

    Version update

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ bl_info = {
     "author" : "Tiago Andrade",
     "description" : "",
     "blender" : (3, 4, 1),
-    "version" : (1, 3, 4),
+    "version" : (1, 3, 5),
     "location" : "Topbar",
     "warning" : "",
     "category" : "Object"
