306680965189e60c61b629984e2a821588b83376
TBA <84085705+TBAndrade@users.noreply.github.com>
2023-02-16 14:29:27 +0000

    Updated version fix not updating

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ bl_info = {
     "author" : "Tiago Andrade",
     "description" : "",
     "blender" : (3, 4, 1),
-    "version" : (1, 5, 3),
+    "version" : (1, 5, 4),
     "location" : "Topbar",
     "warning" : "",
     "category" : "Object"
