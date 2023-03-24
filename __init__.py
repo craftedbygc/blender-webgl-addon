a510c65f2879737e37a3483c9c81edfb5dba7e48
Zala Šeško <zala.sesko@gmail.com>
2023-03-24 10:37:50 +0000

    update version 1.6.1

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ bl_info = {
     "author" : "Tiago Andrade",
     "description" : "",
     "blender" : (3, 4, 1),
-    "version" : (1, 6, 0),
+    "version" : (1, 6, 1),
     "location" : "Topbar",
     "warning" : "",
     "category" : "Object"
