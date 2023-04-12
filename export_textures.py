31ec42d0c3df43f1e2d419dcd9bc7ffc7073eb23
TBA <84085705+TBAndrade@users.noreply.github.com>
2023-04-12 23:35:27 +0100

    Update - normal map included

--- export_textures.py
+++ export_textures.py
@@ -2,6 +2,22 @@ import bpy
 import os
 from . import functions
 
+
+def set_image(folder_path,ob,img,socket_name):
+    if socket_name:
+        new_name = ob.name +"-"+socket_name 
+    else:
+        print(ob.name," - texture not connected to socket")
+        
+    covName = functions.namingConvention(new_name)
+    file_name = covName + ".png"
+    new_file_path = os.path.join(folder_path,file_name)
+    print(new_file_path)
+    img.filepath_raw = new_file_path
+    img.file_format = 'PNG'
+    img.save()
+
+
 def export(folder_path,ob):
     if ob is not None and ob.type == "MESH":
         print("TBA-LEVEL-1")
@@ -16,19 +32,12 @@ def export(folder_path,ob):
                             for link in mat.node_tree.links:
                                 if link.to_node.type == "BSDF_PRINCIPLED" and link.from_node == node:
                                     socket_name = link.to_socket.name
+                                    set_image(folder_path,ob,img,socket_name)
+                                if link.to_node.type == "NORMAL_MAP" and link.from_node == node:
+                                    socket_name = "normal"
+                                    set_image(folder_path,ob,img,socket_name)
 
-                                    if socket_name:
-                                        new_name = ob.name +"-"+socket_name 
-                                    else:
-                                        print(ob.name," - texture not connected to socket")
                                     
-                                    covName = functions.namingConvention(new_name)
-                                    file_name = covName + ".png"
-                                    new_file_path = os.path.join(folder_path,file_name)
-                                    print(new_file_path)
-                                    img.filepath_raw = new_file_path
-                                    img.file_format = 'PNG'
-                                    img.save()
             
         else:
             print(ob.name,"NO MATERIAL DEFINED")
