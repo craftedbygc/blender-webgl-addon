51816aad4534d585dc72a380fbe4ff46831cdd8e
TBA <84085705+TBAndrade@users.noreply.github.com>
2023-04-12 22:18:30 +0100

    small fix to correct center

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ bl_info = {
     "author" : "Tiago Andrade",
     "description" : "",
     "blender" : (3, 4, 1),
-    "version" : (1, 6, 3),
+    "version" : (1, 6, 4),
     "location" : "Topbar",
     "warning" : "",
     "category" : "Object"
